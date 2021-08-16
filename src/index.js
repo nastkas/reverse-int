4cfd07725c4d77f4409dbbc75487367a2124f8a5
nastkas <nastya.by@mail.ru>
2021-08-16 22:29:55 +0300

    init: create solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function rev (n) {
+    n=Math.abs(n);
+    var digits = n.toString().split('');
+    var realDigits = digits.map(Number)
+    const reversed = realDigits.reverse().join();
+    let str = realDigits.join('')
+    
+    return str
 }
+
+
